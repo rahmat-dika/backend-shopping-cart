622e52d91a3bbffe406ab9429b84d8084196dad7
mstkm <mustakimmugi@gmail.com>
2025-02-03 09:10:48 +0700

    feat: change port

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import app from './app';
 import dotenv from "dotenv";
 dotenv.config();
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
