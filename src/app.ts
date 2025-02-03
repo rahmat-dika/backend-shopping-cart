702c64f6683e0937109e6ff995348feb38a3bdee
mstkm <mustakimmugi@gmail.com>
2025-02-03 22:51:27 +0700

    feat: add picture

--- src/app.ts
+++ src/app.ts
@@ -7,11 +7,13 @@ import cartRoutes from "./routes/cartRoutes";
 import orderRoutes from "./routes/orderRoutes";
 import cartItemsRoutes from "./routes/cartItemsRoutes";
 import cors from "cors";
+import path from "path";
 
 const app: Application = express();
 
 app.use(bodyParser.json());
 app.use(cors());
+app.use("/uploads", express.static(path.join(__dirname, "../src/uploads")));
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
